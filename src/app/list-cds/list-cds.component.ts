87696255b91429865c776787ed7329cc8fdcb0c6
Alexandre Ferreira <ferreira64240@hotmail.com>
2023-03-01 09:36:22 +0100

    TP1 ANGULAR suite

--- src/app/list-cds/list-cds.component.ts
+++ src/app/list-cds/list-cds.component.ts
@@ -6,34 +6,38 @@ import { CD } from 'src/app/models/cd';
   templateUrl: './list-cds.component.html',
   styleUrls: ['./list-cds.component.scss']
 })
-export class ListCDsComponent {
-  listcd!: CD;
+export class ListCDsComponent implements OnInit {
 
-  this.listcd = [
-    {
-      id: 1,
-      title: 'The Dark Side of the Moon',
-      author: 'Pink Floyd',
-      price: 10,
-      thumbnail: 'https://upload.wikimedia.org/wikipedia/en/3/3b/Dark_Side_of_the_Moon.png',
-      dateDeSortie: new Date('1973-03-01'),
-      quantite: 10,
-    },{
-      id: 2,
-      title: 'The Wall',
-      author: 'Pink Floyd',
-      price: 5,
-      thumbnail: 'https://upload.wikimedia.org/wikipedia/en/b/bc/Pink_Floyd_-_The_Wall.jpg',
-      dateDeSortie: new Date('1979-11-30'),
-      quantite: 15,
-    },{
-      id: 3,
-      title: 'Wish You Were Here',
-      author: 'Pink Floyd',
-      price: 7,
-      thumbnail: 'https://upload.wikimedia.org/wikipedia/en/0/0b/Pink_Floyd_-_Wish_You_Were_Here.jpg',
-      dateDeSortie: new Date('1975-09-12'),
-      quantite: 7,
-    }
-  ]
+  listcd!: CD[];
+
+  ngOnInit(): void {
+
+    this.listcd = [
+      {
+        id: 1,
+        title: 'The Dark Side of the Moon',
+        author: 'Pink Floyd',
+        price: 10,
+        thumbnail: 'https://upload.wikimedia.org/wikipedia/en/3/3b/Dark_Side_of_the_Moon.png',
+        releaseDate: new Date('1973-03-01'),
+        quantity: 10,
+      }, {
+        id: 2,
+        title: 'The Wall',
+        author: 'Pink Floyd',
+        price: 5,
+        thumbnail: 'https://upload.wikimedia.org/wikipedia/en/b/bc/Pink_Floyd_-_The_Wall.jpg',
+        releaseDate: new Date('1979-11-30'),
+        quantity: 15,
+      }, {
+        id: 3,
+        title: 'Wish You Were Here',
+        author: 'Pink Floyd',
+        price: 7,
+        thumbnail: 'https://upload.wikimedia.org/wikipedia/en/0/0b/Pink_Floyd_-_Wish_You_Were_Here.jpg',
+        releaseDate: new Date('1975-09-12'),
+        quantity: 7,
+      }
+    ];
+  }
 }
