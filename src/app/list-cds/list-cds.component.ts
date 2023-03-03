2ae737728ab8fef30d12e9a66096b0024dd9d225
Alexandre Ferreira <ferreira64240@hotmail.com>
2023-03-03 09:07:58 +0100

    Suite du tp Angular, création de l'affichage de chaque cd

--- src/app/list-cds/list-cds.component.ts
+++ src/app/list-cds/list-cds.component.ts
@@ -15,28 +15,43 @@ export class ListCDsComponent implements OnInit {
     this.listcd = [
       {
         id: 1,
-        title: 'The Dark Side of the Moon',
-        author: 'Pink Floyd',
-        price: 10,
-        thumbnail: 'https://upload.wikimedia.org/wikipedia/en/3/3b/Dark_Side_of_the_Moon.png',
-        releaseDate: new Date('1973-03-01'),
-        quantity: 10,
-      }, {
-        id: 2,
-        title: 'The Wall',
-        author: 'Pink Floyd',
-        price: 5,
-        thumbnail: 'https://upload.wikimedia.org/wikipedia/en/b/bc/Pink_Floyd_-_The_Wall.jpg',
-        releaseDate: new Date('1979-11-30'),
-        quantity: 15,
-      }, {
-        id: 3,
-        title: 'Wish You Were Here',
-        author: 'Pink Floyd',
-        price: 7,
-        thumbnail: 'https://upload.wikimedia.org/wikipedia/en/0/0b/Pink_Floyd_-_Wish_You_Were_Here.jpg',
-        releaseDate: new Date('1975-09-12'),
-        quantity: 7,
+        title : "The Dark Side of the Moon",
+        author : "Pink Floyd",
+        price : 12.99,
+        thumbnail : "https://upload.wikimedia.org/wikipedia/en/3/3b/Dark_Side_of_the_Moon.png",
+        releaseDate : new Date(1973, 3, 1),
+        quantity : 10,
+        onSale : true
+      },
+      {
+        id : 2,
+        title : "The Wall",
+        author : "Pink Floyd",
+        price : 12.99,
+        thumbnail : "https://upload.wikimedia.org/wikipedia/en/0/0b/The_Wall_%28Pink_Floyd_album_-_cover_art%29.png",
+        releaseDate : new Date(1979, 11, 1),
+        quantity : 230,
+        onSale : true
+      },
+      {
+        id : 3,
+        title : "Wish You Were Here",
+        author : "Pink Floyd",
+        price : 12.99,
+        thumbnail : "https://upload.wikimedia.org/wikipedia/en/7/7f/Pink_Floyd_-_Wish_You_Were_Here.png",
+        releaseDate : new Date(1975, 9, 1),
+        quantity : 10,
+        onSale : true
+      },
+      {
+        id : 4,
+        title : "Doolitle",
+        author : "Pixies",
+        price : 12.99,
+        thumbnail : "https://upload.wikimedia.org/wikipedia/en/3/3b/Doolittle_%28Pixies_album_-_cover_art%29.png",
+        releaseDate : new Date(1989, 4, 1),
+        quantity : 10,
+        onSale : true
       }
     ];
   }
--- src/app/models/cd.ts
+++ src/app/models/cd.ts
@@ -1,2 +1,23 @@
 export class CD {
+    id!:number;
+    title!:string;
+    author!:string;
+    price!:number;
+    thumbnail!:string;
+    releaseDate!:Date;
+    quantity!:number;
+    onSale?:boolean;
+    
+    constructor(id:number, title:string, author:string, price:number, thumbnail:string, dateRelease:Date, quantity:number, onSale?:boolean) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.price = price;
+        this.thumbnail = thumbnail;
+        this.releaseDate = dateRelease;
+        this.quantity = quantity;
+        if (onSale != undefined){
+            this.onSale = onSale;
+        }
+    }
 }
